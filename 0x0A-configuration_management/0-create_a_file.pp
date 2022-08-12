2a6f72a05f30484558f8392eb126e3da8d36113d
ChrissLind <chrislindino@gmail.com>
2022-08-12 18:16:48 -0400

    task done

---
+++ 0x0A-configuration_management/0-create_a_file.pp
@@ -0,0 +1,9 @@
+# Create a file in /tmp
+
+file { 'school':
+    path    => '/tmp/school',
+    mode    => '0744',
+    group   => 'www-data',
+    owner   => 'www-data',
+    content => 'I Love Puppet'
+}
