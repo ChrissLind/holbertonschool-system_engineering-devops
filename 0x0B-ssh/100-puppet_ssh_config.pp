9131b2be3abf1844af0f7388092abb800ac8309f
ChrissLind <chrislindino@gmail.com>
2022-08-23 10:00:08 -0400

    moving files

---
+++ 0x0B-ssh/100-puppet_ssh_config.pp
@@ -0,0 +1,13 @@
+#Change to config file
+
+file_line { 'BatchMode':
+ensure => 'present',
+line   => 'PasswordAuthentication no',
+path   => '/etc/ssh/ssh_config'
+}
+
+file_line { 'IdentifyFile':
+ensure => 'present',
+line   => 'IdentityFile ~/.ssh/school',
+path   => '/etc/ssh/ssh_config'
+}
