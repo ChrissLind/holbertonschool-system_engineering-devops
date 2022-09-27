3e1eebe4a0dfbd8a79a16a15eccff231794b2f70
ChrissLind <chrislindino@gmail.com>
2022-09-27 16:57:15 -0400

    testing

--- 0x16-api_advanced/0-subs.py
+++ 0x16-api_advanced/0-subs.py
@@ -7,7 +7,7 @@ def number_of_subscribers(subreddit):
     """Return the number of subscribers on a subreddit."""
     url = "https://www.reddit.com/r/{}/about.json".format(subreddit)
     headers = {
-        "User-Agent": "linux:0x16.api.advanced:v1.0.0 (by /u/bdov_)"
+        "User-Agent": "linux:0x16.api.advanced:v1.0.0 (by /u/MilkyMooseJuice)"
     }
     response = requests.get(url, headers=headers, allow_redirects=False)
     if response.status_code == 404:
