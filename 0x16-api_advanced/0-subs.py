4f83392106294f22870509c1fb4b705b5b39d8e7
ChrissLind <chrislindino@gmail.com>
2022-09-30 10:38:49 -0400

    changing files and doing advanced

--- 0x16-api_advanced/0-subs.py
+++ 0x16-api_advanced/0-subs.py
@@ -7,7 +7,7 @@ def number_of_subscribers(subreddit):
     """Return the number of subscribers on a subreddit."""
     url = "https://www.reddit.com/r/{}/about.json".format(subreddit)
     headers = {
-        "User-Agent": "linux:0x16.api.advanced:v1.0.0 (by /u/MilkyMooseJuice)"
+        "User-Agent": "linux:0x16.api.advanced:v1.0.0"
     }
     response = requests.get(url, headers=headers, allow_redirects=False)
     if response.status_code == 404:
--- 0x16-api_advanced/1-top_ten.py
+++ 0x16-api_advanced/1-top_ten.py
@@ -7,7 +7,7 @@ def top_ten(subreddit):
     """Print the titles of the 10 hottest posts on a subreddit."""
     url = "https://www.reddit.com/r/{}/hot/.json".format(subreddit)
     headers = {
-        "User-Agent": "linux:0x16.api.advanced:v1.0.0 (by /u/)"
+        "User-Agent": "linux:0x16.api.advanced:v1.0.0"
     }
     params = {
         "limit": 10
